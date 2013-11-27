291f67f8705f83a2256ad77cd6e7f490d90a6e3f
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-11-27 17:00:02 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+A4ProjectRevive::Application.config.secret_token = 'a0406a188a9b5c366a1cc69aedfc401e08420674b287dbe168a3bb29243ce16b9f37f5ecc9a86831fa9b7dadf7e4421794bdc40343e34ecf4f1091331519e082'
