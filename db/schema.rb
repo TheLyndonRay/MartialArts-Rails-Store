26d24f65d459552b46759916ebca701e0edde441
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-11-28 04:45:17 -0600

    Edited picturs to 300x300. Added to assets. Created pictures migration for products.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131128001756) do
+ActiveRecord::Schema.define(:version => 20131128104420) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(:version => 20131128001756) do
     t.integer  "stock_quantity"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.string   "pictures"
   end
 
   create_table "provinces", :force => true do |t|
