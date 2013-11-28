fac46663a354e73f3a9189cd5d16b36627959da3
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-11-28 05:29:08 -0600

    Added 15 products with images

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   has_many :orders
   has_many :line_items
 
-  attr_accessible :description, :name, :price, :stock_quantity
+  attr_accessible :description, :name, :price, :stock_quantity, :image
 
   validates :name, :price, :stock_quantity, :presence => true
 end
