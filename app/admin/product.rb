5341d46afb965f24bfa2015f57b90bf3badc98b1
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-11-28 09:35:54 -0600

    Created custom Products index view for ActiveAdmin

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,4 +1,24 @@
 ActiveAdmin.register Product do
 
+  batch_action :flag do |selection|
+    Post.find(selection).each { |p| p.flag! }
+    redirect_to collection_path, :notice => "Posts flagged!"
+  end
+
+  index do
+    column :id
+    column :category_id
+    column :description
+    column :name
+    column :price
+    column :stock_quantity
+    column "Image" do |image|
+      image_tag image.image
+    end
+
+    default_actions
+  end
+
+
   
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   has_many :products
 
   attr_accessible :name
+
+  validates :name, :presence => true
 end
