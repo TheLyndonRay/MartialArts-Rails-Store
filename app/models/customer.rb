de76502295d2bb3437310a59cf8c0706356599e0
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-12-08 20:48:52 -0600

    Added the self.name variable in controller for products

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,8 @@ class Customer < ActiveRecord::Base
   attr_accessible :address, :city, :email, :first_name, :last_name, :postal_code, :province_id
 
   validates :address, :city, :email, :first_name, :last_name, :postal_code, :province_id, :presence => true
+
+  def name
+    self.first_name + ' ' + self.last_name
+  end
 end
